0f72d01cc753ae6130541550d94a96604ace0ef2
陽大　栂野 <haruto3206541070@gmail.com>
2022-07-20 15:29:11 +0900

    add: image

---
+++ firstGIEUITests/Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  HelloNCMB-Bridging-Header.h
+//  HelloNCMB
+//
+//
+
+#ifndef HelloNCMB_Bridging_Header_h
+#define HelloNCMB_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+
+#endif /* HelloNCMB_Bridging_Header_h */
